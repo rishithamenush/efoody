8b59219d25e80c0e25516fe7e4ed9050e5106223
rishitha.menush <rishitha.menush@gmail.com>
2024-08-05 12:32:30 +0530

    Update the app api

---
+++ lib/app/constant.dart
@@ -0,0 +1,4 @@
+
+class Constant {
+  static const String baseUrl = "https://rishmenush.wiremockapi.cloud/";
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,15 @@
+import 'package:efoody/app/constant.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+
+import '../responses/responses.dart';
+part 'app_api.g.dart';
+
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+  
+  @POST("/customers/login")
+  Future<AuthenticationResponse>login();
+}
